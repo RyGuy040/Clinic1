3478f3b8a8062981d9c556eec40ab15a227ea571
RyGuy040 <78233005+RyGuy040@users.noreply.github.com>
2022-01-26 20:04:21 -0500

    Code Moment the Sequel

--- Assets/Scenes/PlayerControl.cs
+++ Assets/Scenes/PlayerControl.cs
@@ -8,11 +8,16 @@ public class PlayerControl : MonoBehaviour
     private float horizontalInput;
     public float playerSpeed = 10.0f;
     public float turnSpeed = 100.0f;
+    private Rigidbody playerRb;
+    public float jumpForce = 8.0f;
+    public float gravityMod;
+    public bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
     }
 
     // Update is called once per frame
@@ -25,6 +30,15 @@ public class PlayerControl : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up * Time.deltaTime * horizontalInput * turnSpeed);
         //jump
-        //shoot
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
     }
 }
