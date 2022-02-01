77c6b2c3e465d0110ac8aaa5404d637a3352410c
RyGuy040 <ryguy040@gmail.com>
2022-02-01 13:21:50 -0500

    shooting controls

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerShooting : MonoBehaviour
 {
+    public GameObject projectilePrefab;
+    public GameObject player;
+    public GameObject fireSpawner;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //Debug.Log("this feature is working right");
+            Instantiate(projectilePrefab, fireSpawner.transform.position, player.transform.rotation);
+        }
     }
 }
